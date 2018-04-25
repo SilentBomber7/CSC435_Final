564c6e13254f4c156143cc62fb5c434951dc0f61
SilentBomber7 <ace71182@gmail.com>
2018-04-25 16:54:29 -0500

    Initial commit

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using myApp1.Models;
+
+namespace myApp1.Controllers
+{
+    public class NotesController : Controller
+    {
+        private assignment7435Entities db = new assignment7435Entities();
+
+        // GET: Notes
+        public ActionResult Index()
+        {
+            var notes = db.Notes.Include(n => n.Category).Include(n => n.User);
+            return View(notes.ToList());
+        }
+
+        // GET: Notes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Note note = db.Notes.Find(id);
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+            return View(note);
+        }
+
+        // GET: Notes/Create
+        public ActionResult Create()
+        {
+            ViewBag.categoryID = new SelectList(db.Categories, "categoryID", "names");
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "email");
+            return View();
+        }
+
+        // POST: Notes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "notesID,title,note1,createdOn,categoryID,isDeleted,UserID")] Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Notes.Add(note);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.categoryID = new SelectList(db.Categories, "categoryID", "names", note.categoryID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "email", note.UserID);
+            return View(note);
+        }
+
+        // GET: Notes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Note note = db.Notes.Find(id);
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.categoryID = new SelectList(db.Categories, "categoryID", "names", note.categoryID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "email", note.UserID);
+            return View(note);
+        }
+
+        // POST: Notes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "notesID,title,note1,createdOn,categoryID,isDeleted,UserID")] Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(note).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.categoryID = new SelectList(db.Categories, "categoryID", "names", note.categoryID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "email", note.UserID);
+            return View(note);
+        }
+
+        // GET: Notes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Note note = db.Notes.Find(id);
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+            return View(note);
+        }
+
+        // POST: Notes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Note note = db.Notes.Find(id);
+            db.Notes.Remove(note);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
